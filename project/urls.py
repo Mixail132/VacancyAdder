ce9fbc8accc91f23c408a7f38b8876c5db65def0
Mixail132 <millionbaxov@gmail.com>
2024-07-01 16:18:16 +0300

    Add new urlpatterns for login and logout user actions.

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,11 @@
+import vadder.views.users as usr
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", include("vadder.urls")),
+    path("admin/", admin.site.urls),
+    path("login/", usr.LoginUser.as_view(), name="login"),
+    path("logout/", usr.logout_user, name="logout"),
 ]
