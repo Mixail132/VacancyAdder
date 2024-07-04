430bc6e972ae4982c8772b32b20e8bf14de5d6af
Mixail132 <millionbaxov@gmail.com>
2024-07-04 14:20:45 +0300

    Add a function to retrieve the data from the vacancy form.

--- vadder/views/views.py
+++ vadder/views/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.http import HttpResponseRedirect
+from django.utils.datastructures import MultiValueDictKeyError
 
 
 def home(request):
@@ -6,3 +8,43 @@ def home(request):
     if request.user.is_authenticated:
         return render(request, "add.html")
     return render(request, "login.html")
+
+
+def getdata(request):
+    """ Gets the vacancy text. """
+    levels = []
+    for level in ["junior", "senior", "middle"]:
+        try:
+            levels.append(request.POST[level])
+        except MultiValueDictKeyError:
+            continue
+    jobtypes = []
+    for jobtype in ["office", "remote", "flexible", "fulltime"]:
+        try:
+            jobtypes.append(request.POST[jobtype])
+        except MultiValueDictKeyError:
+            continue
+    try:
+        english = request.POST["english"]
+    except MultiValueDictKeyError:
+        english = ""
+
+    vacancy_data = {
+        "body": request.POST["body"],
+        "vacancy": request.POST["vacancy"],
+        "profession": request.POST["profession"],
+        "city": request.POST["city"],
+        "experience": request.POST["experience"],
+        "company": request.POST["company"],
+        "contacts": request.POST["contacts"],
+        "english": english,
+        "jobtypes": jobtypes,
+        "levels": levels,
+    }
+
+    for k, v in vacancy_data.items():
+        print(k, ":", v)
+
+    return HttpResponseRedirect(reverse("home"))
+
+
