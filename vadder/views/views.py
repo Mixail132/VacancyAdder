f9593b98289c7ebf524415c6f6f618c6a1d0501e
Mixail132 <millionbaxov@gmail.com>
2024-07-05 15:07:54 +0300

    Organize the lists to a string to prepare the SQL statement,

--- vadder/views/views.py
+++ vadder/views/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, reverse
 from django.http import HttpResponseRedirect
 from django.utils.datastructures import MultiValueDictKeyError
+from datetime import datetime
+from .bases import DataBaseHandler
+from .clears import prepare_vacancy_items
 
 
 def home(request):
@@ -18,33 +21,46 @@ def getdata(request):
             levels.append(request.POST[level])
         except MultiValueDictKeyError:
             continue
+    levels = ", ".join(levels)
+
     jobtypes = []
     for jobtype in ["office", "remote", "flexible", "fulltime"]:
         try:
             jobtypes.append(request.POST[jobtype])
         except MultiValueDictKeyError:
             continue
+    jobtypes = ", ".join(jobtypes)
+
     try:
         english = request.POST["english"]
     except MultiValueDictKeyError:
         english = ""
 
+    today = datetime.now().strftime("%Y-%m-%d %H:%M")
     vacancy_data = {
         "body": request.POST["body"],
         "vacancy": request.POST["vacancy"],
+        "title": request.POST["vacancy"],
         "profession": request.POST["profession"],
         "city": request.POST["city"],
         "experience": request.POST["experience"],
         "company": request.POST["company"],
         "contacts": request.POST["contacts"],
+        "chat_name": request.POST["contacts"],
+        "vacancy_url": request.POST["contacts"],
+        "salary": request.POST["salary"],
         "english": english,
-        "jobtypes": jobtypes,
-        "levels": levels,
+        "job_type": jobtypes,
+        "level": levels,
+        "created_at": today,
+        "time_of_public": today,
     }
+    if "junior" in levels:
+        vacancy_data["profession"] = f'junior, {vacancy_data["profession"]}'
 
-    for k, v in vacancy_data.items():
-        print(k, ":", v)
+    vacancy_keys, vacancy_values = prepare_vacancy_items(vacancy_data)
+
+    database_handler = DataBaseHandler()
+    database_handler.insert_vacancy(vacancy_keys, vacancy_values)
 
     return HttpResponseRedirect(reverse("home"))
-
-
