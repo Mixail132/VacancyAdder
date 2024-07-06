25f2e06697d46d78739d911328d3f851b059bd3e
Mixail132 <millionbaxov@gmail.com>
2024-07-06 13:46:15 +0300

    Allow 'mixail132.pythonanywhere.com' host name.

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-_$++rta((gr+=fvivd&+h6(@t$g)n**@&w*h8cwt1q3ao&qb+s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mixail132.pythonanywhere.com']
 
 
 # Application definition
