a7745449a0b9b5077ae8e151ef5f7a5641a2dbba
Mixail132 <millionbaxov@gmail.com>
2024-07-07 09:25:05 +0300

    Replace some variables to .env file.

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_$++rta((gr+=fvivd&+h6(@t$g)n**@&w*h8cwt1q3ao&qb+s"
+SECRET_KEY = os.getenv("DJ_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mixail132.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = [os.getenv("MY_HOST"), '127.0.0.1']
 
 
 # Application definition
