d06fb75ff1dffb15c188294e081684e8326a54e9
Mixail132 <millionbaxov@gmail.com>
2024-07-13 12:23:00 +0300

    Replace all the application urls to it's own urls.py
    Do it for the ability  to move the application to another Django project.

--- project/urls.py
+++ project/urls.py
@@ -1,12 +1,6 @@
-import vadder.views.users as usr
-import vadder.views.views as act
-from django.contrib import admin
 from django.urls import path, include
 
 
 urlpatterns = [
-    path("", include("vadder.urls")),
-    path("admin/", admin.site.urls, name="admin"),
-    path("login/", usr.LoginUser.as_view(), name="login"),
-    path("logout/", usr.logout_user, name="logout"),
+    path("vadder/", include("vadder.urls")),
 ]
--- vadder/urls.py
+++ vadder/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import views
+import vadder.views.views as views
+import vadder.views.users as users
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("home", views.home, name="home"),
-    path("getdata", views.getdata, name="getdata"),
+    path("home/", views.home, name="home"),
+    path("getdata/", views.getdata, name="getdata"),
+    path("login/", users.LoginUser.as_view(), name="login"),
+    path("logout/", users.logout_user, name="logout"),
+
 ]
