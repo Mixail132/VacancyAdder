2e5f6d2f00ea1a0757cce59032d3d2e2c0379c89
Mixail132 <millionbaxov@gmail.com>
2024-07-17 14:12:06 +0300

    Fix multiple filesystem location.

--- vadder/apps.py
+++ vadder/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
-
+from project.settings import BASE_DIR
 
 class VadderConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "vadder"
+    path = BASE_DIR / "vadder"
