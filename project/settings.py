141923517c4053250f3da627b60f40d905cc22c5
Mixail132 <millionbaxov@gmail.com>
2024-07-22 18:38:45 +0300

    Add the site public url to CSRF TRUSTED ORIGINS.

--- project/settings.py
+++ project/settings.py
@@ -129,3 +129,5 @@ STATIC_ROOT = BASE_DIR / ".local"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = ['https://artistic-myrtie-itacademy-d1b1ce63.koyeb.app']
